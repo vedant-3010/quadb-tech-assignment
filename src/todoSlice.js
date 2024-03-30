0302722dbd426d55652e3bc7b6235a608b396e3d
vedant-3010 <vedantdandgepro@gmail.com>
2024-03-30 14:17:05 +0530

    ⭐Added persistent storage

--- src/todoSlice.js
+++ src/todoSlice.js
@@ -1,22 +1,30 @@
 // todoSlice.js
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = localStorage.getItem('todos')
+  ? JSON.parse(localStorage.getItem('todos'))
+  : [
+        { text: 'Learn React', completed: true },
+        { text: 'Learn Redux', completed: false },
+        { text: 'Build a React App', completed: false },
+  ];
 
 const todoSlice = createSlice({
-  name: "todos",
-  initialState: [
-    { text: "Learn React", completed: true },
-    { text: "Learn Redux", completed: false },
-    { text: "Build a React App", completed: false },
-  ],
+  name: 'todos',
+  initialState,
   reducers: {
     addTodo: (state, action) => {
       state.push(action.payload);
+      localStorage.setItem('todos', JSON.stringify(state));
     },
     removeTodo: (state, action) => {
-      return state.filter((todo, index) => index !== action.payload);
+      const newState = state.filter((todo, index) => index !== action.payload);
+      localStorage.setItem('todos', JSON.stringify(newState));
+      return newState;
     },
     toggleTodo: (state, action) => {
       state[action.payload].completed = !state[action.payload].completed;
+      localStorage.setItem('todos', JSON.stringify(state));
     },
   },
 });
